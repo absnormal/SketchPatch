2d9c1cdaf8ddcd5a1057700107df49bbd1b050b5
Absnormal <absnormal26065873@gmail.com>
2022-01-07 20:20:59 +0800

    [delete] all cut images

---
+++ McFries/convert.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+`convert -resize 109x196! A.png As.png`
+`convert -resize 104x196! B.png Bs.png`
+`convert -resize 108x196! C.png Cs.png`
+`convert -resize 109x196! D.png Ds.png`
+`convert -resize 95x196! E.png Es.png`
+`convert -resize 92x196! F.png Fs.png`
+`convert -resize 114x196! G.png Gs.png`
+`convert -resize 119x196! H.png Hs.png`
+`convert -resize 45x196! I.png Is.png`
+`convert -resize 92x196! J.png Js.png`
+`convert -resize 105x196! K.png Ks.png`
+`convert -resize 90x196! L.png Ls.png`
+`convert -resize 146x196! M.png Ms.png`
+`convert -resize 119x196! N.png Ns.png`
+`convert -resize 115x196! O.png Os.png`
+`convert -resize 105x196! P.png Ps.png`
+`convert -resize 115x196! Q.png Qs.png`
+`convert -resize 103x196! R.png Rs.png`
+`convert -resize 99x196! S.png Ss.png`
+`convert -resize 99x196! T.png Ts.png`
+`convert -resize 108x196! U.png Us.png`
+`convert -resize 106x196! V.png Vs.png`
+`convert -resize 148x196! W.png Ws.png`
+`convert -resize 105x196! X.png Xs.png`
+`convert -resize 100x196! Y.png Ys.png`
+`convert -resize 100x196! Z.png Zs.png`
---
+++ McFries/merge.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+`convert A.png B.png C.png D.png E.png F.png G.png H.png I.png J.png K.png L.png M.png N.png O.png P.png Q.png R.png S.png T.png U.png V.png W.png X.png Y.png Z.png +append all.png`
+`convert As.png Bs.png Cs.png Ds.png Es.png Fs.png Gs.png Hs.png Is.png Js.png Ks.png Ls.png Ms.png Ns.png Os.png Ps.png Qs.png Rs.png Ss.png Ts.png Us.png Vs.png Ws.png Xs.png Ys.png Zs.png +append allW.png`
