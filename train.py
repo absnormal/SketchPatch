67e819739bd5c18774c588b2f090944d5d80ff3d
absnormal <absnormal26065873@gmail.com>
2022-01-08 08:21:44 +0000

    [new] train McNuggets 30 epoch

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 	visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
 	total_iters = 0                # the total number of training iterations
 
-	for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
+	for epoch in range(30): #(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
 		epoch_start_time = time.time()  # timer for entire epoch
 		iter_data_time = time.time()    # timer for data loading per iteration
 		epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
@@ -88,5 +88,6 @@ if __name__ == '__main__':
 			model.save_networks('latest')
 			model.save_networks(epoch)
 
-		print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+		#print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+		print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, 30, time.time() - epoch_start_time))
 		model.update_learning_rate()                     # update learning rates at the end of every epoch.
