0c2c77a2b6264348d10e413ff00328f65377079c
Absnormal <absnormal26065873@gmail.com>
2022-01-09 00:23:23 +0800

    [add] cut bluecurve

--- reference_roboto_reg_1000/white2black.py
+++ reference_roboto_reg_1000/white2black.py
@@ -1,8 +1,8 @@
 import numpy
 import cv2
 
-img = cv2.imread("./all.png", 0)
+img = cv2.imread("./Z.png", 0)
 
 invert = cv2.bitwise_not(img)
 
-cv2.imwrite("./allW.png", invert)
+cv2.imwrite("./Zw.png", invert)
